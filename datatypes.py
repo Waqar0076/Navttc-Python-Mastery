68a8b5b5f1b2dcc702f2ab46baacfa4bde6e3552
Waqar Aamir <113366062+Waqar0076@users.noreply.github.com>
2025-04-05 10:46:46 +0500

    Add files via upload

---
+++ datatypes.py
@@ -0,0 +1,52 @@
+# DataTypes in Python:
+
+# - Numeric Data Types:
+
+age = 20
+print(age)
+print(type(age))
+
+height = 5.12
+print(height)
+print(type(height))
+
+#complex datatype
+i = 1+3j
+print(i)
+print(type(i))
+
+# - Sequence Data Types:
+education = "Intermediate"
+print(education)
+print(type(education))
+
+# Data Structure
+friends = ['ali', 'Ahmed', 'adnan']
+print(friends)
+print(type(friends))
+
+x = ('a', 'b', 'c', 'c')
+print(x)
+print(type(x))
+
+# - Mapping Data Type:
+capital = {
+    'Pakistan' : 'islamabad',
+    'India'    : 'New Delhi'
+}
+print(capital)
+print(type(capital))
+
+# - Boolean Data Type:
+
+check = True
+print(check)
+print(type(check))
+
+
+
+
+
+
+
+

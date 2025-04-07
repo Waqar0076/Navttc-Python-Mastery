58f9ba93b32e09289c10d2c2fbcd9fbf24421118
Waqar Aamir <113366062+Waqar0076@users.noreply.github.com>
2025-04-07 09:42:59 +0500

    Add files via upload

---
+++ user-input.py
@@ -0,0 +1,29 @@
+# taking input from user 
+name = int(input("Enter your Name?")) # input always returns a str
+
+# printing variable through concatenation technique
+print("My Name is", name)
+
+# printing variable through f string technique
+print(f"My Name is {name}")
+
+# checking type of variable
+print(type(name))
+
+# taking user input in int datatype  
+age = int(input("Enter your Age?"))
+print(f"My Age is {age}")
+
+# checking type of variable
+print(type(age))
+
+# taking user input in float datatype  
+height = float(input("Enter your height?"))
+print(f"My height is {height}")
+
+# checking type of variable
+print(type(height))
+
+# print(type(name))
+
+

7614934417e56c205822d939d16adaa8a2f3deeb
Waqar Aamir <113366062+Waqar0076@users.noreply.github.com>
2025-04-11 10:56:26 +0500

    Add files via upload

---
+++ if-else.py
@@ -0,0 +1,36 @@
+# variable naming rules.
+
+# marks = int(input("Enter your marks: "))
+
+# indentations in python
+# if marks < 40: 
+#     print("You are Failed❌")
+
+# elif marks >= 40 and marks <= 50 :
+#     print("You got 'D' Grade")
+
+# elif marks >= 50 and marks <= 60 :
+#     print("You got 'C' Grade")
+
+# elif marks >= 60 and marks <= 70 :
+#     print("You got 'B' Grade")
+
+# elif marks >= 70 and marks <= 80 :
+#     print("You got 'A' Grade")
+
+# elif marks >= 80 :
+#     print("You got 'A+' Grade")
+
+    
+age = int(input("Enter your Age?"))
+
+print("Eligile") if (age >=18) else print("Not Eligile")
+
+# if (age >=18):
+#     print("Eligible for voting....")
+# else:
+#     print("You're not eligible...")
+
+
+# num % 2 == 0 
+

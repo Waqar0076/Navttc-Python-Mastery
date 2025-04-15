455c13f8f2849411ba91c569fcc7c34ffc512a67
Waqar Aamir <113366062+Waqar0076@users.noreply.github.com>
2025-04-15 11:11:07 +0500

    Add files via upload

---
+++ maths.py
@@ -0,0 +1,17 @@
+#importing buildin module
+import math
+
+#returns bottom closest valye
+print(math.floor(8.1))
+
+#returns value closest to 0
+print(math.trunc(-5.3)) 
+
+#roundoff number upward
+print(math.ceil(3.5)) 
+
+#conversions 
+print(int("46", 8))
+print(oct(64))
+
+

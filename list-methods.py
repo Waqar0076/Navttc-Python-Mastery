edf972dcdd869f121ea5cf5b41566f1d918bafda
Waqar Aamir <113366062+Waqar0076@users.noreply.github.com>
2025-04-15 11:09:22 +0500

    Add files via upload

---
+++ list-methods.py
@@ -0,0 +1,24 @@
+# reversing a List
+# my_list = [1,2,3,4,5,6,7]
+# reversed_List = list(reversed(my_list))
+# print(reversed_List)
+
+
+# sorting a List
+# my_list = [1,2,5,8,5,8,7]
+# sorted_List = sorted(my_list)
+# print(sorted_List)
+
+# positive indexing
+my_list = [1, 2, 3, 4, 5, 6]
+print(my_list[1:6:3])
+
+# printing even numbers
+numbers = [2, 3, 4, 5, 6, 7, 8, 9, 10]
+number1 = numbers[ : : 2]
+print(number1)
+
+
+
+
+

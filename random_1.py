68a4790d6ca47186b174f073cd10688a47a898a3
Waqar Aamir <113366062+Waqar0076@users.noreply.github.com>
2025-04-17 11:50:06 +0500

    Add files via upload

---
+++ random_1.py
@@ -0,0 +1,18 @@
+import random
+# generate random number
+print(random.random())
+
+# generate random numbers b/w range
+print(random.randint(1,10))
+
+# choose random elements b/w list
+List1 = ["apple", "banana", "orange"]
+fnfList = random.choice(List1)
+print(fnfList)
+
+#list shuffle
+random.shuffle(List1)
+# print(List1)
+
+
+

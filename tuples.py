bee8e692a4c6122ef6e306cbe42db41259023c4d
Waqar Aamir <113366062+Waqar0076@users.noreply.github.com>
2025-04-18 11:11:57 +0500

    Add files via upload

---
+++ tuples.py
@@ -0,0 +1,20 @@
+# Creating tuple
+Courses = ('Eng', 'Phy', 'Chem')
+
+# access the third item
+# print(Courses[2])
+
+# print('Total Items:', len(Courses))
+
+# Check if an Item Exists in the Tuple
+colors = ('red', 'orange', 'blue')
+print('yellow' in colors)
+
+del Courses
+print(colors)
+del colors
+
+print(colors)
+print(Courses)
+
+

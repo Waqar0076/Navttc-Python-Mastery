a95a523e1d7dc96c33a7f13b9285cea5345d987a
Waqar Aamir <113366062+Waqar0076@users.noreply.github.com>
2025-04-19 12:14:14 +0500

    Add files via upload

---
+++ match-cases.py
@@ -0,0 +1,31 @@
+tech = input("write a technology u want to learn?").title()
+
+match tech:
+    case "Javascript":
+        print("you become a frontend developer")
+
+    case "Python":
+        print("you become a Data Scientist")
+
+    case "Flutter":
+        print("you become a Mobile App developer")
+    
+    case "Selenium":
+        print("you become a block chain specilist")
+
+    case "C#":
+        print("you become a Game Developer")
+
+    case default:
+        print("try again!")
+
+
+
+
+
+
+    
+
+
+
+    
